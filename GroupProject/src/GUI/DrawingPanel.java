fc76e2bd3e0858c1e6e1372c68612b702cc302d7
Daphne <daphnewangnz@gmail.com>
2014-09-27 17:38:42 +1200

    Changed around the files for GUI, and changed how the use of image objects. Eliminated the use of JButtons.

--- GroupProject/src/GUI/DrawingPanel.java
+++ GroupProject/src/GUI/DrawingPanel.java
@@ -1,6 +1,9 @@
 package GUI;
 
+import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JPanel;
 
@@ -9,15 +12,51 @@ import rendering.DrawWorld;
 public class DrawingPanel extends JPanel{
 
 	DrawWorld dw; //this draws all the game-stuff: locations chars items etc
+	StartMenu sm;
+	WindowFrame wf;
+	public static boolean startMenu; //play or menu mode flag
 
-	public DrawingPanel(  ){
+	public DrawingPanel(WindowFrame win){
+		wf = win;
 		dw = new DrawWorld( null, this ); //param: the character, and then a panel
+		sm = new StartMenu( this );
+		startMenu = true; //by default
+
+
 	}
 
+	@Override
 	protected void paintComponent (Graphics g){
-		dw.redraw(g, null, null, null); //param: graphics, room, char, direction
-		
-		
-		//potential changes later: flag for menu mode or play mode, and to have logic 
+
+		if( startMenu ){
+			System.out.println("start menu mode, ready to draw");
+			sm.redraw(g);
+		}
+
+		else{ //else it is in game
+			dw.redraw(g, null, null, null); //param: graphics, room, char, direction
+			//potential changes later: flag for menu mode or play mode, and to have logic
+
+			System.out.println("in game");
+		}
+	}
+
+	@Override
+	public Dimension getPreferredSize() {
+		Dimension dimension = new Dimension(1280, 720);
+		return dimension;
+	}
+
+
+	/*
+	 * Private inner class to handle action listeners - dealing with buttons
+	 */
+
+	private class Handler implements ActionListener{
+		public void actionPerformed(ActionEvent e){
+
+
+
+		}
 	}
 }
--- GroupProject/src/GUI/WindowFrame.java
+++ GroupProject/src/GUI/WindowFrame.java
@@ -4,8 +4,14 @@ package GUI;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -14,34 +20,20 @@ import javax.swing.JPanel;
 
 public class WindowFrame extends JFrame {
 
-	private JPanel mainPanel;
 	private TopMenu topMenu;
-	private JPanel startPanel;
+	private DrawingPanel panel;
 	private Graphics2D graphics; //for leon later?
 	//private Rendering render=new Rendering(); //Leon's object
 	//private Direction: n s e w
-
-	//buttons
-	JButton startButton;
-	JButton loadButton;
-	JButton helpButton;
-
-	//action handler
-	private Handler handler;
 
 	public WindowFrame() {
 		super("Game title");
 		JFrameSetUp();
 		JPanelStuff();
-
-		mainPanel = new JPanel();
-		//add the graphics to the panel
-		buttons();
 	}
 
 	public void JFrameSetUp() {
 		topMenu = new TopMenu(this);
-		handler = new Handler();
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		pack();
 		setSize(1000, 700);
@@ -51,63 +43,20 @@ public class WindowFrame extends JFrame {
 
 	}
 
-	public void refreshGraphics(){
+/*	public void refreshGraphics(){
 		//render.redraw(graphics, room, character, direction); //TO BE FILLED LATER: we get room and char from client: jimmy
-	}
+	}*/
 
 	public void JPanelStuff() {
-		startPanel = new JPanel( new BorderLayout() );
-		startPanel.setBackground(Color.WHITE);
+		System.out.println("In JPanel setups");
 
-		this.getContentPane().add(startPanel);
-	}
+		panel = new DrawingPanel(this);
 
-	public void buttons(){
-		ImageIcon start = new ImageIcon(WindowFrame.class.getResource("startMenuImages/start.png"));
-		ImageIcon load = new ImageIcon(WindowFrame.class.getResource("startMenuImages/load.png"));
-		ImageIcon help = new ImageIcon(WindowFrame.class.getResource("startMenuImages/help.png"));
+		add(panel, BorderLayout.CENTER); // add canvas
 
-		startButton = new JButton(start);
-		loadButton = new JButton(load);
-		helpButton = new JButton(help);
+		setVisible(true); // make sure we are visible!
 
-		setButton(startButton);
-
-
-	}
-
-	public void setButton(JButton button){
-		button.setBorder(BorderFactory.createEmptyBorder());
-		button.setContentAreaFilled(false);
-		button.setLocation(1000, 500);
-		button.addActionListener(handler);
-
-
-		startPanel.add(button, BorderLayout.EAST);
-		startPanel.validate();
-
-	}
-
-
-
-
-
-
-	/*
-	 * Private inner class to handle action listeners - dealing with buttons
-	 */
-
-	private class Handler implements ActionListener{
-		public void actionPerformed(ActionEvent e){
-
-			if(e.getSource()==startButton){
-
-				System.out.println("Pressed start button");
-			}
-
-
-
-		}
+		panel.repaint(); //repaint susses the graphics object
 	}
 
 
