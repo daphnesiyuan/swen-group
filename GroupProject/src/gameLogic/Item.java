29647f3bcb2c0e4b652d1c03de7efa24b7564ca6
griffiryan <griffiryan@vivant.ecs.vuw.ac.nz>
2014-09-29 19:40:30 +1300

    test.

--- GroupProject/src/gameLogic/Item.java
+++ GroupProject/src/gameLogic/Item.java
@@ -3,15 +3,9 @@ package gameLogic;
 import java.util.List;
 
 public abstract class Item {
-
-
-
 public abstract String getDescription();
 public abstract Tile2D getTile();
 public abstract boolean moveItemTo(Tile2D toTile);
 public abstract int getWeight();
 public abstract String interactWith(Avatar avatar);
-
-
-
 }
