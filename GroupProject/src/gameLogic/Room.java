ec9bd8cce37c1776a125aeca9734b62c2a5d6fbe
griffiryan <griffiryan@vivant.ecs.vuw.ac.nz>
2014-09-29 19:31:32 +1300

    Code Tidying.

--- GroupProject/src/gameLogic/Room.java
+++ GroupProject/src/gameLogic/Room.java
@@ -6,17 +6,8 @@ import java.util.List;
 public class Room {
 
 	Tile2D[][] tiles;
-	List <? extends Item> items;
+	List <Item> items;
 	List<Avatar> characters;
-
-	public List<Avatar> getCharacters() {
-		return characters;
-	}
-
-	public void setCharacters(List<Avatar> characters) {
-		this.characters = characters;
-	}
-
 	List<Door> doors;
 	List<Floor> floors;
 	List<Floor> spawns;
@@ -26,20 +17,11 @@ public class Room {
 
 
 
-	public Room(int r, Tile2D[][] roomTiles, List<? extends Item> roomitems) {
-		roomNumber = r;
-		tiles = roomTiles;
-		items = roomitems;
+	public Room(int roomNumber, Tile2D[][] tiles, List<Item> items) {
+		this.roomNumber = roomNumber;
+		this.tiles = tiles;
+		this.items = items;
 	}
-
-	public Item getItemAt(int x, int y){
-		return tiles[x][y].getTopItem();
-	}
-
-	public Tile2D[][] getTiles(){
-		return tiles;
-	}
-
 
 	public boolean checkValidCharacterMove(Avatar mover, Tile2D move) {
 
@@ -55,39 +37,50 @@ public class Room {
 		// if there is an Item in the move position - return false;
 		if(move.canMoveTo()==false) return false;
 
-
 		return true;
+	}
+
+	public Item getItemAt(int x, int y){
+		return tiles[x][y].getTopItem();
+	}
+
+	public Tile2D[][] getTiles(){
+		return tiles;
+	}
+
+	public List<Avatar> getCharacters() {
+		return characters;
+	}
+
+	public void setCharacters(List<Avatar> characters) {
+		this.characters = characters;
 	}
 
 	public void setDoors(List<Door> roomDoors) {
 		doors = roomDoors;
-
 	}
 
 	public void setFloors(List<Floor> roomFloors) {
 		floors = roomFloors;
-
 	}
 
 	public void setSpawns(List<Floor> roomSpawns) {
 		spawns = roomSpawns;
-
 	}
 
 	public void setWalls(List<Wall> roomWalls) {
 		walls = roomWalls;
-
 	}
 
 	public int getRoomNumber(){
 		return roomNumber;
 	}
 
-	public List<? extends Item> getItems() {
+	public List<Item> getItems() {
 		return items;
 	}
 
-	public void setItems(List<? extends Item> items) {
+	public void setItems(List<Item> items) {
 		this.items = items;
 	}
 
@@ -114,6 +107,4 @@ public class Room {
 	public void setRoomNumber(int roomNumber) {
 		this.roomNumber = roomNumber;
 	}
-
-
 }
