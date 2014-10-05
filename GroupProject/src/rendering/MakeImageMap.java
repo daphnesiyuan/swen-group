f1663628a112834a922e2e5516c45d9ec8a3ae6e
leonnorth <leonnorth@gmail.com>
2014-10-05 20:14:10 +1300

    This is a big one: Added a class that will make a map of string to buffered image. Added a bunch of art work for animation. Added a FloatingPointer class that animates a floating pointer above the players head.

---
+++ GroupProject/src/rendering/MakeImageMap.java
@@ -0,0 +1,77 @@
+package rendering;
+
+import gameLogic.Avatar;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.imageio.ImageIO;
+
+public class MakeImageMap {
+
+	static Map<String, BufferedImage> images = new HashMap<String, BufferedImage>();
+	static int avatarAnimations = 4;
+	static int tileAnimations = 2;
+	static int directions = 4;
+	static int pointerAnimations = 6;
+
+	public MakeImageMap() {
+
+	}
+
+	public static Map<String, BufferedImage> makeMap(){
+		for(int j = 0; j < directions; j++){
+			for(int i = 0; i < avatarAnimations; i++){
+				//Avatar A
+				String avatarAName = "AvatarA"+Direction.get(j)+""+i;
+				java.net.URL avatarAURL = Rendering.class.getResource(avatarAName + ".png");
+				addToMap(avatarAName, avatarAURL);
+
+				//		Avatar B
+				String avatarBName = "AvatarB"+Direction.get(j)+""+i;
+				java.net.URL avatarBURL = Rendering.class.getResource(avatarBName + ".png");
+				addToMap(avatarBName, avatarBURL);
+			}
+		}
+
+		for(int i = 0; i < tileAnimations; i++){
+			//wall
+			String wallName = "Wall"+i;
+			java.net.URL wallURL = Rendering.class.getResource(wallName + ".png");
+			addToMap(wallName, wallURL);
+
+			//floor
+			String floorName = "Floor"+i;
+			java.net.URL floorURL = Rendering.class.getResource(floorName + ".png");
+			addToMap(floorName, floorURL);
+
+			//door
+			String doorName = "Door"+i;
+			java.net.URL doorURL = Rendering.class.getResource(doorName + ".png");
+			addToMap(doorName, doorURL);
+		}
+
+		for(int i = 0; i < pointerAnimations; i++){
+			String pointerName = "FloatingPointer"+i;
+			java.net.URL pointerURL = Rendering.class.getResource(pointerName + ".png");
+			addToMap(pointerName, pointerURL);
+		}
+		return images;
+
+	}
+
+	private static void addToMap(String name, java.net.URL imageURL){
+
+		System.out.println(name);
+		BufferedImage img = null;
+		try {
+			img = ImageIO.read(imageURL);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		images.put(name, img);
+	}
+
+}
