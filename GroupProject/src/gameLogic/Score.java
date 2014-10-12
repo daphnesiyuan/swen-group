ee43c6fa41e5f78744e1d955cd3ed7ff7acb1122
leonnorth <leonnorth@gmail.com>
2014-10-12 17:18:53 +1300

    Adding class to last push

---
+++ GroupProject/src/gameLogic/Score.java
@@ -0,0 +1,20 @@
+package gameLogic;
+
+import java.util.Map;
+
+/**
+ *
+ * @author northleon
+ *
+ */
+public class Score {
+
+	public Score() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Map<String, Integer> getScore(){
+		return null;
+	}
+
+}
