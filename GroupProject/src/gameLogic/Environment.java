a2c9430879fee9d875072cfe7167434e8ca8e086
griffiryan <griffiryan@lazeeza.ecs.vuw.ac.nz>
2014-10-14 18:47:34 +1300

    Test Push.

--- GroupProject/src/gameLogic/Environment.java
+++ GroupProject/src/gameLogic/Environment.java
@@ -1,6 +1,8 @@
 package gameLogic;
 
+import java.awt.Color;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Environment extends Thread implements Serializable{
@@ -13,12 +15,29 @@ public class Environment extends Thread implements Serializable{
 
 	private int count = 0;
 
+	private List<String> keyColors;
+	private List<String> doorColors;
+
 	// items in the game are identified using unique item ids.
 	private int itemID;
 
 	public Environment(Game game){
 		this.game = game;
 		this.itemID = 0;
+
+		this.doorColors = new ArrayList<String>();
+		this.keyColors = new ArrayList<String>();
+
+		keyColors.add("R");
+		keyColors.add("G");
+		keyColors.add("Y");
+		keyColors.add("P");
+
+		doorColors.add("R");
+		doorColors.add("G");
+		doorColors.add("Y");
+		doorColors.add("P");
+
 
 	}
 
@@ -28,7 +47,7 @@ public class Environment extends Thread implements Serializable{
 			if(count == 0){		// initial environment item generation
 				for(int i = 1; i < 5; i++){		// at start - put lights in all rooms but arena.
 					genLight(i);
-					//genKey(i);
+					genKey(i);
 				}
 
 
@@ -37,14 +56,32 @@ public class Environment extends Thread implements Serializable{
 			}
 		}
 	}
-
-
 	private void genKey(int i){
 		Room room = game.getRoomsInGame().get(i);
-		Key key = new Key(room.getTiles()[1][5]);
-		key.setItemID(itemID++);
-		room.getTiles()[1][5].addItem(key);
-
+		if(keyColors.get(0).equals("R")){
+			RedKey key  = new RedKey(room.getTiles()[1][5]);
+			key.setItemID(itemID++);
+			room.getTiles()[1][5].addItem(key);
+			keyColors.remove(0);
+		}
+		else if(keyColors.get(0).equals("G")){
+			GreenKey key = new GreenKey(room.getTiles()[1][5]);
+			key.setItemID(itemID++);
+			room.getTiles()[1][5].addItem(key);
+			keyColors.remove(0);
+		}
+		else if(keyColors.get(0).equals("Y")){
+			YellowKey key = new YellowKey(room.getTiles()[1][5]);
+			key.setItemID(itemID++);
+			room.getTiles()[1][5].addItem(key);
+			keyColors.remove(0);
+		}
+		else if(keyColors.get(0).equals("P")){
+			PurpleKey key = new PurpleKey(room.getTiles()[1][5]);
+			key.setItemID(itemID++);
+			room.getTiles()[1][5].addItem(key);
+			keyColors.remove(0);
+		}
 	}
 
 	private void genLight(int i){
